45819f5591236ba799527e9dc7195d4d6d7c8cb5
MohamedAmine-BenSalah <mohamedaminebensalah6@gmail.com>
2021-10-10 23:34:29 +0100

    final

--- main.js
+++ main.js
@@ -4,6 +4,10 @@ var user = [
         password : "student"
     }
 ]
+function getInfo(){
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('pwd').value
+}
 function createQuestion(){
     var value = document.getElementById('Question').value;
     window.localStorage.setItem("Question",value);
